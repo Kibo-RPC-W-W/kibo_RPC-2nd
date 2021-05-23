9ee6e644e5c1cdc49547c34033e98d613d795193
Tim Huang <taxihuang030926@gmail.com>
2021-05-23 21:49:03 +0800

    參考了一些東西

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/kibo_RPC_2nd/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/kibo_RPC_2nd/YourService.java
@@ -1,22 +1,39 @@
 package jp.jaxa.iss.kibo.rpc.kibo_RPC_2nd;
 
-import android.graphics.Bitmap;
-
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Reader;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.DetectorParameters;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import static org.opencv.android.Utils.bitmapToMat;
+import static org.opencv.android.Utils.matToBitmap;
+import static org.opencv.core.CvType.CV_32FC1;
+import static org.opencv.core.CvType.CV_64F;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import gov.nasa.arc.astrobee.Kinematics;
+import gov.nasa.arc.astrobee.Result;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import static android.graphics.Bitmap.createBitmap;
+import android.graphics.Bitmap;
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
@@ -26,13 +43,8 @@ import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
  */
 public class YourService extends KiboRpcService {
 
-    static int pat;
-    float test = 0;
-    static float patf,px, py, pz;
-
     Point a_ = new Point();
     Quaternion q = new Quaternion();
-
 
     @Override
     protected void runPlan1() {
@@ -50,38 +62,114 @@ public class YourService extends KiboRpcService {
         api.moveTo(z, q, true);
         api.moveTo(a_, q, true);
     }
-    public  static <BufferedImage> void QRReader(Bitmap bitmap) throws IOException, NotFoundException {
-        MultiFormatReader formatReader = new MultiFormatReader();
-        //讀取指定的二維碼文件
-        byte[] arr = bitmapToArray(bitmap);
-        LuminanceSource source = new PlanarYUVLuminanceSource(arr, bitmap.getWidth(), bitmap.getHeight(), 0, 0, bitmap.getWidth(), bitmap.getHeight(), false);
-
-        BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
-        com.google.zxing.Result qrResult = formatReader.decode(bBitmap);
-        //輸出相關的二維碼信息
-        System.out.println(qrResult.getText());
-        String format = qrResult.getText();
-        String regex = "-?\\d*\\.?\\d*";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(format);
-        float mf = Float.parseFloat(m.group());
+    private void moveToWrapper(double pos_x,double pos_y,double pos_z,
+                               double qua_x,double qua_y,double qua_z,
+                               double qua_w){
+        //指定した座標に移動させる
+        //Moving and orientating Astrobee according to the coordinates and quaternions inputted above
+        final int LOOP_MAX = 20;
+        final Point point = new Point(pos_x,pos_y,pos_z);
+        final Quaternion quaternion = new Quaternion((float)qua_x,(float)qua_y,
+                (float)qua_z,(float)qua_w);
+        Result result;
+        int loop_count = 0;
+        do{
+            result = api.moveTo(point,quaternion,true);
+            Point position = getPositionWrapper(pos_x,pos_y,pos_z);
+            double m_pos_x = position.getX();
+            double m_pos_y = position.getY();
+            double m_pos_z = position.getZ();
+            if ((pos_y <= -9.4) && m_pos_x >= (pos_x - 0.01) && m_pos_x <= (pos_x + 0.01) && m_pos_y >= (pos_y - 0.01) && m_pos_y <= (pos_y + 0.01) && m_pos_z >= (pos_z - 0.01) && m_pos_z <= (pos_z + 0.01)) {
+                break;
+            }  else if (m_pos_y < (pos_y + 0.05)) {
+                break;
+            }else{
+                loop_count++;
+            }
+        }while(!result.hasSucceeded() && loop_count < LOOP_MAX);
     }
 
-    public static byte[] bitmapToArray(Bitmap bmp){
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 50, stream);
-        byte[] byteArray = stream.toByteArray();
-        return byteArray;
+
+    private void moveToQR(int QR_num) {
+        //指定したQRコードに移動させる
+        //Moving Astrobee to the QR code
+        double pos_table[][] = {{10.71, -5.59, 4.52, 0, 0, 1, 0},
+                {11.16, -7.98, 5.47, 0.5, 0.5, -0.5, 0.5}};
+        Point point = new Point(pos_table[QR_num][0], pos_table[QR_num][1], pos_table[QR_num][2]);
+        Quaternion quaternion = new Quaternion((float) pos_table[QR_num][3], (float) pos_table[QR_num][4],
+                (float) pos_table[QR_num][5], (float) pos_table[QR_num][6]);
+        Result result;
+        int loop_count = 0;
+        final int LOOP_MAX = 20;
+        double m_pos_x;
+        double m_pos_y;
+        double m_pos_z;
+        do {
+            result = api.moveTo(point,quaternion,true);
+            if (QR_num == 0) {
+                Point position = getPositionWrapper(10.71, -5.59, 4.52);
+                m_pos_x = position.getX();
+                m_pos_y = position.getY();
+                m_pos_z = position.getZ();
+            } else {
+                Point position = getPositionWrapper(11.16, -7.98, 5.47);
+                m_pos_x = position.getX();
+                m_pos_y = position.getY();
+                m_pos_z = position.getZ();
+            }
+            if (QR_num == 0 && m_pos_x >= 10.7 && m_pos_x <= 10.72 && m_pos_y >= -5.6 && m_pos_y <= -5.58 && m_pos_z >= 4.51 && m_pos_z <= 4.53) {
+                break;
+            }else if (QR_num == 1 && m_pos_x >= 11.15 && m_pos_x <= 11.17 && m_pos_y >= -7.99 && m_pos_y <= -7.97 && m_pos_z >= 5.46 && m_pos_z <= 5.48){
+                break;
+            } else {
+                loop_count++;
+            }
+        } while (!result.hasSucceeded() && loop_count < LOOP_MAX);
     }
 
-    @Override
-    protected void runPlan2(){
-        // write here your plan 2
+
+    private void relativeMoveToWrapper(double pos_x,double pos_y,double pos_z,
+                                       double qua_x,double qua_y,double qua_z,
+                                       double qua_w){
+        //指定した相対的な座標に移動させる
+        //Move to the coordinates relative to Astrobee (and orientate)
+        final int LOOP_MAX = 20;
+        final Point point = new Point(pos_x,pos_y,pos_z);
+        final Quaternion quaternion = new Quaternion((float)qua_x,(float)qua_y,
+                (float)qua_z,(float)qua_w);
+        Result result;
+        int loop_count = 0;
+        do{
+            result = api.relativeMoveTo(point,quaternion,true);
+            loop_count++;
+        }while(!result.hasSucceeded() && loop_count < LOOP_MAX);
     }
 
-    @Override
-    protected void runPlan3(){
-        // write here your plan 3
+
+    private void sleep(int millis){
+        //Astrobeeを停止させる
+        //Stopping Astrobee for __ milliseconds
+        try {
+            Thread.sleep(millis);
+        }catch(InterruptedException e){
+        }
+    }
+
+
+    private Point getPositionWrapper(double def_pos_x,double def_pos_y,double def_pos_z){
+        //現在位置を取得する
+        //Acquiring the real-time data for Astrobee’s orientation and coordinates
+        int timeout_sec = 10;
+        Kinematics kinematics;
+        kinematics = api.getTrustedRobotKinematics(timeout_sec);
+        Point ans_point = null;
+        if(kinematics != null){
+            ans_point = kinematics.getPosition();
+        }
+        if(ans_point == null){
+            ans_point = new Point(def_pos_x,def_pos_y,def_pos_z);
+        }
+        return ans_point;
     }
 
 }
