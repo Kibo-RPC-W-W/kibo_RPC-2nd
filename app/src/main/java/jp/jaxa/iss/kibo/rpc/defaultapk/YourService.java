b64eb40bead630df5a11ae44a6a4b089808003d4
Yu_Yue <9987jdvrb@gmail.com>
2021-05-23 15:48:23 +0800

    merge

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,15 +1,31 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import com.google.zxing.EncodeHintType;
+
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
 
+/**
+ * A(11.21, -9.8, 4.79)
+ */
 public class YourService extends KiboRpcService {
+    Point a_ = new Point();
+    Quaternion q = new Quaternion();
     @Override
-    protected void runPlan1(){
+    protected void runPlan1() {
         // write here your plan 1
         api.startMission();
+        api.moveTo(new Point(11.21, -9.8, 4.79), new Quaternion(0, 0, 0.707f, -0.707f), true);
+    }
+
+    public void pattern2(){
+        Point z = new Point(11.21, -9.8, a_.getZ());
+        api.moveTo(z, q, true);
+        api.moveTo(a_, q, true);
 
     }
 
