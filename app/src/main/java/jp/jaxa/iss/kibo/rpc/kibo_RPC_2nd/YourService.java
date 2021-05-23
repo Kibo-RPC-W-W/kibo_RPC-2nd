28b7f546c1fd667e4bd97659044c1d9abfc5b391
Tim Huang <taxihuang030926@gmail.com>
2021-05-23 20:48:55 +0800

    add QR scan

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/kibo_RPC_2nd/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/kibo_RPC_2nd/YourService.java
@@ -3,7 +3,6 @@ package jp.jaxa.iss.kibo.rpc.kibo_RPC_2nd;
 import android.graphics.Bitmap;
 
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
@@ -11,18 +10,13 @@ import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
-import gov.nasa.arc.astrobee.Kinematics;
-import gov.nasa.arc.astrobee.Result;
-import gov.nasa.arc.astrobee.types.Point;
-import gov.nasa.arc.astrobee.types.Quaternion;
-import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
-import jp.jaxa.iss.kibo.rpc.api.types.PointCloud;
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
@@ -31,8 +25,15 @@ import jp.jaxa.iss.kibo.rpc.api.types.PointCloud;
  * A(11.21, -9.8, 4.79)
  */
 public class YourService extends KiboRpcService {
+
+    static int pat;
+    float test = 0;
+    static float patf,px, py, pz;
+
     Point a_ = new Point();
     Quaternion q = new Quaternion();
+
+
     @Override
     protected void runPlan1() {
         // write here your plan 1
@@ -55,11 +56,15 @@ public class YourService extends KiboRpcService {
         byte[] arr = bitmapToArray(bitmap);
         LuminanceSource source = new PlanarYUVLuminanceSource(arr, bitmap.getWidth(), bitmap.getHeight(), 0, 0, bitmap.getWidth(), bitmap.getHeight(), false);
 
-        BinaryBitmap bbitmap = new BinaryBitmap(new HybridBinarizer(source));
-        com.google.zxing.Result result = formatReader.decode(bbitmap);
+        BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
+        com.google.zxing.Result qrResult = formatReader.decode(bBitmap);
         //輸出相關的二維碼信息
-        System.out.println( "二維碼文本內容："+ result.getText());
-
+        System.out.println(qrResult.getText());
+        String format = qrResult.getText();
+        String regex = "-?\\d*\\.?\\d*";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(format);
+        float mf = Float.parseFloat(m.group());
     }
 
     public static byte[] bitmapToArray(Bitmap bmp){
